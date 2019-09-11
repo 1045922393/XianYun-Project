462d5f35fe5553eb1b4531aee9a8f0205f71a90b
wannianxiong <179073176@qq.com>
2019-09-11 17:50:42 +0800

    新建攻略酒店首页

---
+++ pages/hotel/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="hotel">
+    <h1>hotel</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/post/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="post">
+    <h1>攻略</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
