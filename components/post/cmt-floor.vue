05b4aa7296e81be2c82f887376cdae6cc720c5bf
lqs <123456789@qq.com>
2019-09-15 18:00:16 +0800

    差递归

---
+++ components/post/cmt-floor.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="cmt-floor">
+        <div class="cmt-content">
+            <div class="cmt-info">
+                <i></i>
+                <span></span>
+            </div>
+            <p class="cmt-message"></p>
+            <el-row type="flex">
+            </el-row>
+            <div class="cmt-ctrl">
+                <a href="">回复</a>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="less" scoped>
+
+</style>
